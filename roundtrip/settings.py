f382ca07dede158aa784b57254ada578205dcb2c
AnYa <84224953+anyahuang@users.noreply.github.com>
2022-01-03 14:40:21 +0100

    Update settings.py

--- roundtrip/settings.py
+++ roundtrip/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ql8+jlbud)a5743fg@0s!5yn-26i@=s3fln1sdx5a7zii4yfy0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
